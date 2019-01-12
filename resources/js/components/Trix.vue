6bdde586fa17b3c5ad2f955216de5ca0a14d86dc
Javier <fjmartinez@disitec.es>
2019-01-12 17:11:35 +0100

    stop propagation keydown in trix.

--- resources/js/components/Trix.vue
+++ resources/js/components/Trix.vue
@@ -1,20 +1,20 @@
 <template>
-    <trix-editor
-        ref="theEditor"
-        @trix-change="handleChange"
-        @trix-initialize="initialize"
-        @trix-attachment-add="handleAddFile"
-        @trix-attachment-remove="handleRemoveFile"
-        @trix-file-accept="handleFileAccept"
-        :value="value"
-        :placeholder="placeholder"
-        class="trix-content"
-    />
+  <trix-editor
+    ref="theEditor"
+    @keydown.stop
+    @trix-change="handleChange"
+    @trix-initialize="initialize"
+    @trix-attachment-add="handleAddFile"
+    @trix-attachment-remove="handleRemoveFile"
+    @trix-file-accept="handleFileAccept"
+    :value="value"
+    :placeholder="placeholder"
+    class="trix-content"
+  />
 </template>
 
 <script>
-// import Trix from 'trix';
-import 'trix/dist/trix.css';
+import 'trix/dist/trix.css'
 
 export default {
   name: 'trix-r64',
@@ -28,26 +28,26 @@ export default {
 
   methods: {
     initialize() {
-      this.$refs.theEditor.editor.insertHTML(this.value);
+      this.$refs.theEditor.editor.insertHTML(this.value)
     },
 
     handleChange() {
-      this.$emit('change', this.$refs.theEditor.value);
+      this.$emit('change', this.$refs.theEditor.value)
     },
 
     handleFileAccept(e) {
       if (!this.withFiles) {
-        e.preventDefault();
+        e.preventDefault()
       }
     },
 
     handleAddFile(event) {
-      this.$emit('file-add', event);
+      this.$emit('file-add', event)
     },
 
     handleRemoveFile(event) {
-      this.$emit('file-remove', event);
+      this.$emit('file-remove', event)
     }
   }
-};
+}
 </script>
