5cc6009b06552ea5dfa55afcb940d8d2a58d362c
Francisco Javier Martinez <fjmartinez@disitec.es>
2019-06-29 16:11:32 +0200

    add sum field to row (#107)

--- resources/js/components/row/DetailField.vue
+++ resources/js/components/row/DetailField.vue
@@ -19,6 +19,7 @@
           v-if="shouldShowHeading"
           :fields="field.fields"
           :base-classes="field.childConfig"
+          :heading-classes="field.headingClasses"
         />
         <div
           v-for="row in values"
@@ -35,6 +36,12 @@
             :parent-attribute="field.attribute"
           />
         </div>
+        <SumField
+          v-if="shouldShowSum"
+          :values="values"
+          :field="field"
+          :fields="field.fields"
+        />
       </template>
     </r64-excerpt>
   </r64-panel-item>
@@ -45,11 +52,12 @@ import R64Field from '../../mixins/R64Field'
 import RowHeading from './RowHeading'
 import RowField from './RowField'
 import DetailFieldItem from './DetailFieldItem'
+import SumField from './SumField'
 
 export default {
   mixins: [RowField, R64Field],
 
-  components: { RowHeading, DetailFieldItem },
+  components: { RowHeading, DetailFieldItem, SumField },
 
   props: ['resource', 'resourceName', 'resourceId', 'field']
 }
