63ffe16719947771db2fa6039d42d706cc88cc5d
Javier Martinez <javi@64robots.com>
2021-08-13 14:59:11 +0200

    Show table row on index view (#209)

--- resources/js/components/row/IndexField.vue
+++ resources/js/components/row/IndexField.vue
@@ -1,9 +1,23 @@
 <template>
-    <span>{{ field.value }}</span>
+  <div class="p-4">
+    <a v-if="show" href="#" @click="show = false">Hide {{ field.name }}</a>
+    <a v-else href="#" @click="show = true">Show {{ field.name }}</a>
+    <TableWrapper v-if="show" :field="field" />
+  </div>
 </template>
 
 <script>
+import TableWrapper from './TableWrapper.vue'
+
 export default {
-    props: ['resourceName', 'field'],
+  props: ['resourceName', 'field'],
+
+  components: { TableWrapper },
+
+  data() {
+    return {
+      show: false
+    }
+  }
 }
 </script>
