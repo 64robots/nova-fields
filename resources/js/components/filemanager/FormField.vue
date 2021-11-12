9c8c315d04f9ba3ce1bf981470b4d3ebf6baf65a
Parth KaPatel <parth.kapatel@arsenaltech.com>
2021-11-12 18:28:10 +0530

    changes in filemanager

--- resources/js/components/filemanager/FormField.vue
+++ resources/js/components/filemanager/FormField.vue
@@ -7,76 +7,76 @@
       :wrapper-classes="wrapperClasses"
       :label-classes="labelClasses"
   >
-        <template slot="field">
+    <template slot="field">
 
-            <template v-if="field.value && field.display == 'image'">
-                <div class="card relative card relative border-lg border-50 overflow-hidden px-0 py-0 max-w-xs mb-2">
-                    <template v-if="field.type == 'image'">
-                        <ImageDetail class="block w-full" :file="field" :css="''"></ImageDetail>
-                    </template>
+      <template v-if="field.value && field.display == 'image'">
+        <!--                <div class="card relative card relative border-lg border-50 overflow-hidden px-0 py-0 max-w-xs mb-2">
+                            <template v-if="field.type == 'image'">
+                                <ImageDetail class="block w-full" :file="field" :css="''"></ImageDetail>
+                            </template>
 
-                    <template v-else>
-                        <object class="no-preview" v-html="field.image">
-                        </object>
-                    </template>
-                </div>
-            </template>
+                            <template v-else>
+                                <object class="no-preview" v-html="field.image">
+                                </object>
+                            </template>
+                        </div>-->
+      </template>
 
-            <modal-filemanager
-                ref="filemanager"
-                :resource="resourceName"
-                :name="field.attribute"
-                :home="field.home"
-                :active="openModal"
-                :currentPath="currentPath"
-                :defaultFolder="defaultFolder"
-                :filter="field.filterBy"
-                :buttons="field.buttons"
-                v-on:open-modal="openModalCreateFolder"
-                v-on:close-modal="closeFilemanagerModal"
-                v-on:update-current-path="updateCurrentPath"
-                v-on:showInfoItem="showInfoItem"
-                v-on:uploadFiles="uploadFiles"
-                :value="value"
-            />
+      <modal-filemanager
+          ref="filemanager"
+          :resource="resourceName"
+          :name="field.attribute"
+          :home="field.home"
+          :active="openModal"
+          :currentPath="currentPath"
+          :defaultFolder="defaultFolder"
+          :filter="field.filterBy"
+          :buttons="field.buttons"
+          v-on:open-modal="openModalCreateFolder"
+          v-on:close-modal="closeFilemanagerModal"
+          v-on:update-current-path="updateCurrentPath"
+          v-on:showInfoItem="showInfoItem"
+          v-on:uploadFiles="uploadFiles"
+          :value="value"
+      />
 
-            <DetailPopup
-                ref="detailPopup"
-                :info="info"
-                :active="activeInfo"
-                :popup="true"
-                :buttons="field.buttons"
-                v-on:closePreview="closePreview"
-                v-on:refresh="refreshCurrent"
-                v-on:selectFile="setValue"
-                v-on:rename="fileRenamed"
-            />
+      <DetailPopup
+          ref="detailPopup"
+          :info="info"
+          :active="activeInfo"
+          :popup="true"
+          :buttons="field.buttons"
+          v-on:closePreview="closePreview"
+          v-on:refresh="refreshCurrent"
+          v-on:selectFile="setValue"
+          v-on:rename="fileRenamed"
+      />
 
-            <create-folder ref="createFolderModal" :active="showCreateFolder" :current="currentPath" v-on:closeCreateFolderModal="closeModalCreateFolder" v-on:refresh="refreshCurrent" />
+      <create-folder ref="createFolderModal" :active="showCreateFolder" :current="currentPath" v-on:closeCreateFolderModal="closeModalCreateFolder" v-on:refresh="refreshCurrent" />
 
-            <UploadProgress ref="uploader" :current="currentPath" :visibility="field.visibility" :rules="field.upload_rules" v-on:removeFile="removeFileFromUpload"></UploadProgress>
+      <UploadProgress ref="uploader" :current="currentPath" :visibility="field.visibility" :rules="field.upload_rules" v-on:removeFile="removeFileFromUpload"></UploadProgress>
 
-            <file-select :id="field.name" :classes="inputClasses" :field="field" :is-readonly="field.readonly" :css="errorClasses"  v-model="value" v-on:open-modal="openFilemanagerModal"></file-select>
+      <file-select :id="field.name" :classes="inputClasses"   :url="selectImageUrl"  :field="field" :is-readonly="field.readonly" :css="errorClasses"  v-model="value" v-on:open-modal="openFilemanagerModal"></file-select>
 
-            <p class="mt-3 flex items-center text-sm btn-link" v-if="value">
-                <button type="button" class="cursor-pointer dim btn btn-link text-primary inline-flex items-center" @click="openRemoveModal">
-                    <icon type="delete" view-box="0 0 20 20" width="16" height="16" />
-                    <span class="class ml-2 mt-1">
-                        {{__('Delete')}}
-                    </span>
-                </button>
-            </p>
+      <!--            <p class="mt-3 flex items-center text-sm btn-link" v-if="value">
+                      <button type="button" class="cursor-pointer dim btn btn-link text-primary inline-flex items-center" @click="openRemoveModal">
+                          <icon type="delete" view-box="0 0 20 20" width="16" height="16" />
+                          <span class="class ml-2 mt-1">
+                              {{__('Delete')}}
+                          </span>
+                      </button>
+                  </p>-->
 
-            <confirm-modal-remove-file
-                :active="removeModalOpen"
-                @confirm="removeFile"
-                @close="closeRemoveModal"></confirm-modal-remove-file>
+      <confirm-modal-remove-file
+          :active="removeModalOpen"
+          @confirm="removeFile"
+          @close="closeRemoveModal"></confirm-modal-remove-file>
 
-            <p v-if="hasError" class="my-2 text-danger">
-                {{ firstError }}
-            </p>
-        </template>
-    </r64-default-field>
+      <p v-if="hasError" class="my-2 text-danger">
+        {{ firstError }}
+      </p>
+    </template>
+  </r64-default-field>
 </template>
 
 <script>
@@ -97,157 +97,158 @@ import api from '../../api';
 
 
 export default {
-    mixins: [FormField, HandlesValidationErrors,R64Field],
+  mixins: [FormField, HandlesValidationErrors,R64Field],
 
-    props: ['resourceName', 'resourceId', 'field'],
+  props: ['resourceName', 'resourceId', 'field'],
 
-    components: {
-        'file-select': FileSelect,
-        'modal-filemanager': ModalFileManager,
-        'create-folder': CreateFolderModal,
-        DetailPopup: DetailPopup,
-        UploadProgress: UploadProgress,
-        'confirm-modal-remove-file': ConfirmModalRemoveFile,
-        ImageDetail: ImageDetail,
-        'rename-modal': RenameModal,
-        'confirm-modal-delete': ConfirmModalDelete,
+  components: {
+    'file-select': FileSelect,
+    'modal-filemanager': ModalFileManager,
+    'create-folder': CreateFolderModal,
+    DetailPopup: DetailPopup,
+    UploadProgress: UploadProgress,
+    'confirm-modal-remove-file': ConfirmModalRemoveFile,
+    ImageDetail: ImageDetail,
+    'rename-modal': RenameModal,
+    'confirm-modal-delete': ConfirmModalDelete,
+  },
+
+  data: () => ({
+    openModal: false,
+    showCreateFolder: false,
+    defaultFolder: null,
+    currentPath: '/',
+
+    //modalFile
+    info: {},
+    activeInfo: false,
+    popupDetailsLoaded: false,
+
+    //uploader
+    filesToUpload: {},
+    uploadType: null,
+    folderUploadedName: null,
+    selectImageUrl : '',
+    removeModalOpen: false,
+  }),
+
+  methods: {
+    openModalCreateFolder() {
+      this.showCreateFolder = true;
+    },
+    closeModalCreateFolder() {
+      this.showCreateFolder = false;
     },
 
-    data: () => ({
-        openModal: false,
-        showCreateFolder: false,
-        defaultFolder: null,
-        currentPath: '/',
-
-        //modalFile
-        info: {},
-        activeInfo: false,
-        popupDetailsLoaded: false,
-
-        //uploader
-        filesToUpload: {},
-        uploadType: null,
-        folderUploadedName: null,
-
-        removeModalOpen: false,
-    }),
-
-    methods: {
-        openModalCreateFolder() {
-            this.showCreateFolder = true;
-        },
-        closeModalCreateFolder() {
-            this.showCreateFolder = false;
-        },
-
-        refreshCurrent() {
-            this.$refs.filemanager.getData(this.currentPath);
-        },
-
-        openFilemanagerModal() {
-            this.setCurrentPath();
-            this.openModal = true;
-        },
-
-        closeFilemanagerModal() {
-            this.openModal = false;
-        },
-
-        updateCurrentPath(val) {
-            this.currentPath = val;
-        },
-
-        showInfoItem(item) {
-            this.activeInfo = true;
-            this.info = item;
-        },
-
-        closePreview() {
-            this.info = {};
-            this.activeInfo = false;
-            this.popupDetailsLoaded = false;
-        },
-
-        uploadFiles(files, type, firstFolderName) {
-            this.filesToUpload = files;
-            this.uploadType = type;
-            this.folderUploadedName = firstFolderName;
-            this.$refs.uploader.startUploadingFiles(files, type);
-        },
-
-        removeFileFromUpload(uploadedFileId) {
-            let index = this.filesToUpload.map(item => item.id).indexOf(uploadedFileId);
-
-            this.$delete(this.filesToUpload, index);
-            if (this.filesToUpload.length === 0) {
-                if (this.uploadType == 'folders') {
-                    this.callFolderEvent(this.folderUploadedName);
-                }
-
-                this.folderUploadedName = null;
-                this.uploadType = null;
-
-                this.refreshCurrent();
-            }
-        },
-
-        setCurrentPath() {
-            if (this.field.folder != null) {
-                this.defaultFolder = this.field.folder;
-                this.currentPath = this.field.folder;
-            } else {
-                this.defaultFolder = '/';
-                this.currentPath = '/';
-            }
-        },
-
-        removeFile() {
-            this.field.value = null;
-            this.value = '';
-            this.removeModalOpen = false;
-        },
-
-        fileRenamed(item) {
-            this.info = item;
-        },
-
-        openRemoveModal() {
-            this.removeModalOpen = true;
-        },
-
-        closeRemoveModal() {
-            this.removeModalOpen = false;
-        },
-
-        callFolderEvent(path) {
-            api.eventFolderUploaded(this.currentPathFolder + '/' + path);
-        },
-
-        /*
-         * Set the initial, internal value for the field.
-         */
-        setInitialValue() {
-            this.value = this.field.value || '';
-        },
-
-        /**
-         * Fill the given FormData object with the field's internal value.
-         */
-        fill(formData) {
-            formData.append(this.field.attribute, this.value || '');
-        },
-
-        /**
-         * Update the field's internal value.
-         */
-        setValue(file) {
-            this.value = file.path;
-            this.closeFilemanagerModal();
-        },
+    refreshCurrent() {
+      this.$refs.filemanager.getData(this.currentPath);
     },
 
-    created() {
-        this.setCurrentPath();
+    openFilemanagerModal() {
+      this.setCurrentPath();
+      this.openModal = true;
     },
+
+    closeFilemanagerModal() {
+      this.openModal = false;
+    },
+
+    updateCurrentPath(val) {
+      this.currentPath = val;
+    },
+
+    showInfoItem(item) {
+      this.activeInfo = true;
+      this.info = item;
+    },
+
+    closePreview() {
+      this.info = {};
+      this.activeInfo = false;
+      this.popupDetailsLoaded = false;
+    },
+
+    uploadFiles(files, type, firstFolderName) {
+      this.filesToUpload = files;
+      this.uploadType = type;
+      this.folderUploadedName = firstFolderName;
+      this.$refs.uploader.startUploadingFiles(files, type);
+    },
+
+    removeFileFromUpload(uploadedFileId) {
+      let index = this.filesToUpload.map(item => item.id).indexOf(uploadedFileId);
+
+      this.$delete(this.filesToUpload, index);
+      if (this.filesToUpload.length === 0) {
+        if (this.uploadType == 'folders') {
+          this.callFolderEvent(this.folderUploadedName);
+        }
+
+        this.folderUploadedName = null;
+        this.uploadType = null;
+
+        this.refreshCurrent();
+      }
+    },
+
+    setCurrentPath() {
+      if (this.field.folder != null) {
+        this.defaultFolder = this.field.folder;
+        this.currentPath = this.field.folder;
+      } else {
+        this.defaultFolder = '/';
+        this.currentPath = '/';
+      }
+    },
+
+    removeFile() {
+      this.field.value = null;
+      this.value = '';
+      this.removeModalOpen = false;
+    },
+
+    fileRenamed(item) {
+      this.info = item;
+    },
+
+    openRemoveModal() {
+      this.removeModalOpen = true;
+    },
+
+    closeRemoveModal() {
+      this.removeModalOpen = false;
+    },
+
+    callFolderEvent(path) {
+      api.eventFolderUploaded(this.currentPathFolder + '/' + path);
+    },
+
+    /*
+     * Set the initial, internal value for the field.
+     */
+    setInitialValue() {
+      this.value = this.field.value || '';
+    },
+
+    /**
+     * Fill the given FormData object with the field's internal value.
+     */
+    fill(formData) {
+      formData.append(this.field.attribute, this.value || '');
+    },
+
+    /**
+     * Update the field's internal value.
+     */
+    setValue(file) {
+      this.value = file.path;
+      this.selectImageUrl = file.url;
+      this.closeFilemanagerModal();
+    },
+  },
+
+  created() {
+    this.setCurrentPath();
+  },
 };
 </script>
--- resources/js/components/filemanager/custom/FileSelect.vue
+++ resources/js/components/filemanager/custom/FileSelect.vue
@@ -1,40 +1,52 @@
 <template>
-    <div class="flex flex-wrap items-stretch w-full mb-2 relative">
-        <input type="text" class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 form-control form-input form-input-bordered-l relative"
-               :placeholder="__('Select a file')" :class="classes" v-model="value" :disabled="isReadonly">
-        <div class="flex -mr-px">
-            <span class="filemanager-open flex items-center leading-normal rounded-lg rounded-l-none border border-l-0 border-grey-light bg-40 px-3 whitespace-no-wrap text-grey-dark text-sm cursor-pointer" @click="openModalFilemanager">{{ __('Open FileManager') }}</span>
-        </div>  
-    </div>  
+  <div class="filemanager-image-block flex flex-row border-lg border-50 px-0 py-0 max-w-xs mb-2">
+    <div v-if="field.url || url" class="image-block card flex justify-center w-full h-5/6 px-2">
+      <img v-if="!url && field.url" :src="field.url" class="image block w-full self-center" draggable="false">
+      <img v-if="url" :src="url" class="image block w-full self-center" draggable="false">
+    </div>
+    <div class="items-stretch w-full mb-2 relative pt-1">
+      <div class="flex -mr-px max-w-xs">
+        <button type="button" class="filemanager-open flex items-center leading-normal rounded border border-grey-light bg-40 px-3 whitespace-no-wrap text-grey-dark text-sm cursor-pointer" @click="openModalFilemanager">{{ __('Open FileManager') }}</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import ImageDetail from '../../../modules/Image';
+import ModalFileManager from "../../ModalFileManager";
+import CreateFolderModal from "../../CreateFolderModal";
+import DetailPopup from "../../DetailPopup";
+import UploadProgress from "../../UploadProgress";
+import ConfirmModalRemoveFile from "../../ConfirmModalRemoveFile";
 export default {
-    props: ['value', 'field', 'isReadonly','classes'],
-
-    methods: {
-        openModalFilemanager() {
-            this.$emit('open-modal');
-        },
+  props: ['value', 'field', 'isReadonly','classes','url'],
+  components: {
+    ImageDetail: ImageDetail,
+  },
+  methods: {
+    openModalFilemanager() {
+      this.$emit('open-modal');
     },
+  },
 };
 </script>
 
 <style scoped>
 .form-input-bordered-l {
-    background-color: var(--white);
-    border-width: 1px;
-    border-color: var(--60);
-    padding-left: 0.75rem;
-    padding-right: 0.75rem;
-    color: var(--80);
-    border-radius: 0.5rem;
-    -webkit-box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.05);
-    border-bottom-right-radius: initial;
-    border-top-right-radius: initial;
+  background-color: var(--white);
+  border-width: 1px;
+  border-color: var(--60);
+  padding-left: 0.75rem;
+  padding-right: 0.75rem;
+  color: var(--80);
+  border-radius: 0.5rem;
+  -webkit-box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.05);
+  border-bottom-right-radius: initial;
+  border-top-right-radius: initial;
 }
 
 .filemanager-open {
-    border-color: var(--60);
+  border-color: var(--60);
 }
 </style>
