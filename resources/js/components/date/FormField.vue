d86d84d3946498453211c6554b0695627a42af17
Parth KaPatel <parth.kapatel@arsenaltech.com>
2022-01-20 14:42:25 +0530

    add clear value icon in date and date time field

--- resources/js/components/date/FormField.vue
+++ resources/js/components/date/FormField.vue
@@ -2,49 +2,52 @@
   <r64-default-field
       :hide-field="hideField"
       :field="field"
+      :show-help-text="showHelpText"
       :hide-label="hideLabelInForms"
       :field-classes="fieldClasses"
       :wrapper-classes="wrapperClasses"
       :label-classes="labelClasses"
   >
     <template slot="field">
-      <date-time-picker
-          ref="dateTimePicker"
-          :dusk="field.attribute"
-          :name="field.name"
-          :value="value"
-          dateFormat="Y-m-d"
-          :alt-format="pickerDisplayFormat"
-          :placeholder="placeholder"
-          :enable-time="false"
-          :enable-seconds="false"
-          :first-day-of-week="firstDayOfWeek"
-          :class="[errorClasses, inputClasses]"
-          @change="handleChange"
-          :disabled="isReadonly"
-      />
+      <div class="flex items-center">
+        <date-time-picker
+            ref="dateTimePicker"
+            :dusk="field.attribute"
+            :name="field.name"
+            :value="value"
+            dateFormat="Y-m-d"
+            :alt-format="pickerDisplayFormat"
+            :placeholder="placeholder"
+            :enable-time="false"
+            :enable-seconds="false"
+            :first-day-of-week="firstDayOfWeek"
+            :class="[errorClasses, inputClasses]"
+            @change="handleChange"
+            :disabled="isReadonly"
+        />
 
-      <a
-          v-if="field.nullable"
-          @click.prevent="$refs.dateTimePicker.clear()"
-          href="#"
-          :title="__('Clear value')"
-          tabindex="-1"
-          class="p-1 px-2 cursor-pointer leading-none focus:outline-none"
-          :class="{
-            'text-50': !value.length,
-            'text-black hover:text-danger': value.length,
-          }"
-      >
-        <icon type="x-circle" width="22" height="22" viewBox="0 0 22 22" />
-      </a>
+        <a
+            v-if="field.nullable"
+            @click.prevent="$refs.dateTimePicker.clear()"
+            href="#"
+            :title="__('Clear value')"
+            tabindex="-1"
+            class="p-1 px-2 cursor-pointer leading-none focus:outline-none"
+            :class="{
+              'text-50': !value.length,
+              'text-black hover:text-danger': value.length,
+            }"
+        >
+          <icon type="x-circle" width="22" height="22" viewBox="0 0 22 22" />
+        </a>
 
-      <p
-          v-if="hasError"
-          class="my-2 text-danger"
-      >
-        {{ firstError }}
-      </p>
+        <p
+            v-if="hasError"
+            class="my-2 text-danger"
+        >
+          {{ firstError }}
+        </p>
+      </div>
     </template>
   </r64-default-field>
 </template>
