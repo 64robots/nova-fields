7c63bb599d23200548df3a8d3eee786480859033
Parth KaPatel <parth.kapatel@arsenaltech.com>
2022-12-12 14:32:47 +0530

    update changes for radio button

--- resources/js/components/radio-button/DetailField.vue
+++ resources/js/components/radio-button/DetailField.vue
@@ -1,56 +1,55 @@
 <template>
   <panel-item
-
       :field="field"
       :hide-label="hideLabelInDetail"
       :label-classes="panelLabelClasses"
       :field-classes="panelFieldClasses"
       :wrapper-classes="panelWrapperClasses"
   >
-      <slot  name="value">
-        <p class="text-90" :title="this.field.value" :aria-label="this.field.value">{{ getOptionLabel(value) }}</p>
-        <span v-if="hasOptionHint(value)" :class="[errorClasses, inputClasses]" class="radio-hint mt-1 block text-sm text-80 leading-normal">{{ getOptionHint(value) }}</span>
-      </slot>
+    <template #value>
+      <p class="text-90" :title="this.field.value" :aria-label="this.field.value">{{ getOptionLabel(value) }}</p>
+      <span v-if="hasOptionHint(value)" :class="[errorClasses, inputClasses]" class="radio-hint mt-1 block text-sm text-80 leading-normal">{{ getOptionHint(value) }}</span>
+    </template>
   </panel-item>
 </template>
 
 <script>
 import R64Field from '../../mixins/R64Field'
-    import HasOptions from '../../mixins/HasOptions';
-    import CanToggle from '../../mixins/CanToggle';
-    import PanelItem from '../PanelItem.vue';
+import HasOptions from '../../mixins/HasOptions';
+import CanToggle from '../../mixins/CanToggle';
+import PanelItem from '../PanelItem.vue';
 
-    export default {
-        mixins: [HasOptions, CanToggle,R64Field],
+export default {
+  mixins: [HasOptions, CanToggle,R64Field],
 
-        props: ['field', 'fieldName'],
+  props: ['field', 'fieldName'],
 
-        computed: {
-            label() {
-                return this.fieldName || this.field.name
-            },
-            rawValue() {
-                return this.field.value;
-            },
-            value() {
-                if (this.field.skipTransformation) {
-                    return this.field.value;
-                }
+  computed: {
+    label() {
+      return this.fieldName || this.field.name
+    },
+    rawValue() {
+      return this.field.value;
+    },
+    value() {
+      if (this.field.skipTransformation) {
+        return this.field.value;
+      }
 
-                const displayValue = this.field.options[this.field.value];
+      const displayValue = this.field.options[this.field.value];
 
-                if (displayValue) {
-                    return displayValue
-                }
+      if (displayValue) {
+        return displayValue
+      }
 
-                return this.field.default;
-            }
-        },
+      return this.field.default;
     }
+  },
+}
 </script>
 
 <style>
-    .mlbz-hidden {
-        display: none !important;
-    }
+.mlbz-hidden {
+  display: none !important;
+}
 </style>
